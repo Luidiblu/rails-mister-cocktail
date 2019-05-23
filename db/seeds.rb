c9590e7aeb9cd1cc862dae05c3c9812c18f2e889
Diego Pisani <diego.luidiblu@gmail.com>
2019-05-23 18:35:07 -0300

    Helll Yeaaaaaaaaaaaaaah

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning records'
+Cocktail.destroy_all
+puts "Cocktails: #{Cocktail.count}"
+Ingredient.destroy_all
+puts "Ingredients: #{Ingredient.count}"
+Dose.destroy_all
+puts "Doses: #{Dose.count}"
+puts '-' * 100
+
+puts 'Started the Seed'
+
+puts 'Will attempt to scrape https://gruetwinery.com/7/cocktail-recipes'
+
+html_file = open('https://gruetwinery.com/7/cocktail-recipes').read
+html_doc = Nokogiri::HTML(html_file)
+
+html_doc.search('.cocktail h3').each do |cocktail|
+  cocktail_name = cocktail.text.strip
+  new_cocktail = Cocktail.new(name: cocktail_name)
+  if new_cocktail.valid?
+    new_cocktail.save
+    puts new_cocktail.name
+  else
+    puts 'Something went wrong'
+  end
+end
+
+puts "Ended and you now are the proud owner of #{Cocktail.count} cocktails!"
